c6d6a9c77df51b9d5d92d050eac1a0c32700bc87
Kyrylo Dyachenko <104459315+kirill25460@users.noreply.github.com>
2023-04-19 20:41:46 +0300

    2

--- my-app/App.js
+++ my-app/App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, } from "react";
+import {} from "react-native";
+import * as Font from "expo-font";
+import AppLoading from 'expo-app-loading';
+
+import LoginScreen from './Screens/LoginScreen';
+import RegistrationScreen from './Screens/RegistartionScreen';
+
+const loadApplication = async () => {
+  await Font.loadAsync({
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+  });
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [isReady, setIsReady] = useState(false);
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={loadApplication}
+        onFinish={() => setIsReady(true)}
+        onError={console.warn}
+      />
+    );
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <>
+    <LoginScreen />
+    {/* { <RegistrationScreen /> } */}
+    </>
+ );
+}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ my-app/Screens/LoginScreen.jsx
@@ -0,0 +1,190 @@
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+   Dimensions,
+} from "react-native";
+
+
+const initialState = {
+  email: "",
+  password: "",
+};
+
+
+
+export default function LoginScreen() {
+  console.log(Platform.OS);
+
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [state, setstate] = useState(initialState);
+
+  const [dimensions, setdimensions] = useState(
+    Dimensions.get("window").width - 16 * 2
+  );
+
+  useEffect(() => {
+    const onChange = () => {
+      const width = Dimensions.get("window").width - 16 * 2;
+
+      setdimensions(width);
+    };
+    Dimensions.addEventListener("change", onChange);
+    return () => {
+      Dimensions.removeEventListener("change", onChange);
+    };
+  }, []);
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    console.log(state);
+    setstate(initialState);
+  };
+ 
+  return (
+    <TouchableWithoutFeedback onPress={keyboardHide}>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.image}
+          source={require("../assets/photoBG.png")}
+        >
+          <KeyboardAvoidingView
+            // behavior={Platform.OS == "ios" ? "padding" : "height"}
+          >
+            <View
+              style={{
+                ...styles.form,
+                marginBottom: isShowKeyboard ? 30 : 110,
+                width: dimensions,
+              }}
+            >
+              <View style={styles.header}>
+                <Text style={styles.headerTitle}>Войти</Text>
+              </View>
+              <View>
+
+                <TextInput
+                  style={styles.input}
+                  onFocus={() => setIsShowKeyboard(true)}
+                  value={state.email}
+                  placeholder="Адрес электронной почты"
+                  onChangeText={(value) =>
+                    setstate((prevState) => ({ ...prevState, email: value }))
+                  }
+                />
+              </View>
+              <View style={{ marginTop: 20 }}>
+
+                <TextInput
+                  style={styles.input}
+                  color='#BDBDBD'
+                  secureTextEntry={true}
+                  onFocus={() => setIsShowKeyboard(true)}
+                  value={state.password}
+                  placeholder="Введите свой пароль"
+                  onChangeText={(value) =>
+                    setstate((prevState) => ({ ...prevState, password: value }))
+                  }
+                />
+              </View>
+              <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.btn}
+                onPress={keyboardHide}
+              >
+                <Text style={styles.btnTitle}>Войти</Text>
+              </TouchableOpacity>
+              <View style={styles.authFooter}>
+                  <Text style={styles.switchText}>Нет аккаунта? Зарегистрироваться</Text>
+              </View>
+            </View>
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+    },
+    image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",  // justifyContent: "center",
+    alignItems: "center",
+    },
+  
+    input: {
+    borderWidth: 1,
+    borderColor: "#e8e8e8",
+    height: 50,
+    borderRadius: 8,
+    backgroundColor: '#f6f6f6',
+    padding: 15,
+    marginBottom: 16,
+    color: "#212121",
+    },
+  
+    form: {
+    marginHorizontal: 40,
+    },
+  
+    inputTitle: {
+    color: "#f0f8ff",
+    marginBottom: 10,
+    fontSize: 18,
+    fontFamily: "Roboto-Medium",
+    },
+  
+    btn: {
+    marginTop: 43,
+    marginBottom: 16,
+    alignItems: "center",
+    paddingTop: 16,
+    paddingBottom: 16,
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    },
+  
+    btnTitle: {
+    color: Platform.OS === "ios" ? "#4169e1" : "#f0f8ff",
+    fontSize: 16,
+    fontFamily: "Roboto-Regular",
+    },
+  
+    header: {
+    alignItems: "center",
+    marginBottom: 33,
+    },
+  
+    headerTitle: {
+    fontSize: 30,
+    color: "#f0f8ff",
+    fontFamily: "Roboto-Medium",
+    },
+  
+    authFooter: {
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    },
+    switchText: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    color:'#1b4371'
+    },
+  
+  });
