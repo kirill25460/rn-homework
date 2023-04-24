539fa4e5f1491d885c792956fec30b5c1b8138ac
Kyrylo Dyachenko <104459315+kirill25460@users.noreply.github.com>
2023-04-24 18:26:13 +0300

    3

--- my-app/App.js
+++ my-app/App.js
@@ -16,6 +16,7 @@ const loadApplication = async () => {
 
 export default function App() {
   const [iasReady, setIasReady] = useState(false);
+  const routing = useRoute(false);
   if (!iasReady) {
     return (
       <AppLoading
